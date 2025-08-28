6fa9c60b54340d54c9d99484fb6e6fdbf0d82c8a
04042015 <m.iggopramulia14@gmail.com>
2025-08-29 00:16:01 +0700

    Update App.tsx

--- publik/src/App.tsx
+++ publik/src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-// import CategoryPage from "./pages/CategoryPage"; // ✅ tambahkan import
-// import PostPage from "./pages/PostPage"; // ✅ import baru
+import CategoryPage from "./pages/CategoryPage"; // ✅ tambahkan import
+import PostPage from "./pages/PostPage"; // ✅ import baru
 
 const queryClient = new QueryClient();
 
