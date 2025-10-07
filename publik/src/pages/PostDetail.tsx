afb55907eacaf453575f2a4e68981fc37a66668d
04042015 <m.iggopramulia14@gmail.com>
2025-10-08 01:42:40 +0700

    Update PostDetail.tsx

--- publik/src/pages/PostDetail.tsx
+++ publik/src/pages/PostDetail.tsx
@@ -258,7 +258,7 @@ const PostDetail = () => {
       <img 
         src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linktree.svg" 
         alt="Copy Link" 
-        className="w-4 h-4 invert" />
+        className="w-4 h-4 invert"
       />
     </button>
   </div>
@@ -484,7 +484,6 @@ const PostDetail = () => {
             className="bg-gray-600 hover:bg-gray-700 text-white p-3 rounded-full flex items-center justify-center"
           >
             <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linktree.svg" alt="Copy Link" className="w-4 h-4 invert" />        
-          </a>
           </button>
         </div>
       )}
